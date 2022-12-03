e5f83ef6f139ca60e376fb44b3a73dcf30b1fc96
Tim Moerkerken <tim@clinicalgraphics.com>
2022-12-03 14:39:40 +0100

    Rewrite day-2 part 1 to facilitate part 2

--- day-2/solve.py
+++ day-2/solve.py
@@ -1,16 +1,16 @@
 # %%
 from pathlib import Path
 import numpy as np
-# %%
-# Loading input
+# %% Loading input
 with open(Path(__file__).parent / "input.txt", "r") as fh:
     input_data = fh.read()
 input_data_flat = input_data.split("\n")
-# Rule scoring maps
+
+# %% Rule maps
 shape_points = {
-    "X": 1,  # Rock
-    "Y": 2,  # Paper
-    "Z": 3,  # Scissors
+    "A": 1,  # Rock
+    "B": 2,  # Paper
+    "C": 3,  # Scissors
 }
 
 outcome_points = {
@@ -19,5 +19,60 @@ outcome_points = {
     "win": 6,
 }
 
-# %% Part 1: Score according to guide
+win_lookup = {
+    "A": "C",  # Rock beats scissors
+    "B": "A",  # Paper beats rock
+    "C": "B",  # Scissors beat paper
+}
 
+loss_lookup = {
+    "A": "B",  # Rock loses to paper
+    "B": "C",  # Paper loses to scissors
+    "C": "A",  # Scissors lose to rock
+}
+
+shape_to_presumed = {
+    "A": "X",
+    "B": "Y",
+    "C": "Z",
+}
+
+presumed_to_shape = {v:k for k, v in shape_to_presumed.items()}
+
+# %% Part 1: Score according to guide
+points = 0
+
+def get_outcome_from_entries(opponent_entry, my_entry):
+    if (win_lookup[my_entry] == opponent_entry):
+        return "win"
+    elif (my_entry == opponent_entry):
+        return "draw"
+    else:
+        return "loss"
+
+for round in input_data_flat:
+    if len(round_entries := round.split(" ")) == 2:
+        opponent_entry, my_entry_presumed = round_entries
+        my_entry = presumed_to_shape[my_entry_presumed]
+
+        points += shape_points[my_entry]
+        outcome = get_outcome_from_entries(opponent_entry, my_entry)
+
+        points += outcome_points[outcome]
+
+# %% Part 2: Forcing outcomes
+def get_my_entry_for_outcome(outcome, opponent_entry):
+    opponent_entry, my_entry = round_entries
+
+    if outcome == "win":
+        return ...
+    elif outcome == "loss":
+        return ...
+    else:
+        return ...
+
+for round in input_data_flat:
+    if len(round_entries := round.split(" ")) == 2:
+        opponent_entry, outcome = round_entries
+
+# %%
