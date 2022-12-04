24cf72bb83c7972be22d1e4a390a8cfbb8356a9b
Tim Moerkerken <tim.moerkerken@gmail.com>
2022-12-04 19:52:15 +0100

    Add part 2

--- day-3/solve.py
+++ day-3/solve.py
@@ -1,11 +1,17 @@
 # %%
 from pathlib import Path
 import numpy as np
+from functools import reduce
 import string
 # %% Loading input
 with open(Path(__file__).parent / "input.txt", "r") as fh:
     input_data = fh.read()
 input_data_flat = input_data.split("\n")
+# Filter out newline
+input_data_flat =  np.array(
+    [np.array(list(i)) for i in input_data_flat if len(i)],
+    dtype=object
+)
 # %% Part 1
 priority_lookup = dict(zip(
     list(string.ascii_lowercase[:26]
@@ -16,14 +22,20 @@ priority_lookup = dict(zip(
 priority_sum = 0
 for items in input_data_flat:
     print(items)
-    comp_a_items, comp_b_items = np.array(
-        list(items)
-    ).reshape(2, -1)
+    comp_a_items, comp_b_items = items.reshape(2, -1)
 
     duplicate_item = np.intersect1d(comp_a_items, comp_b_items)
     assert len(duplicate_item) == 1
     duplicate_item = duplicate_item[0]
 
     priority_sum += priority_lookup[duplicate_item]
+
+# %% Part 2
+priority_sum = 0
+for group in np.array(input_data_flat).reshape(-1, 3):
+    badge = reduce(np.intersect1d, group)
+    assert len(badge) == 1
+    badge = badge[0]
+    priority_sum += priority_lookup[badge]
 
 # %%
